8849a03bf7fb2dd0a7d4eaffb4dc1bfd09965e44
Patrice Tisserand <p.tisserand@gmail.com>
2022-12-05 18:06:31 +0100

    day04: part 2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -19,13 +19,30 @@ fn contains(low_a: i32, high_a: i32, low_b: i32, high_b: i32) -> bool {
     return false;
 }
 
-fn parse_pairs(re: &regex::Regex, line: &str) -> u32 {
+fn overlap(low_a: i32, high_a: i32, low_b: i32, high_b: i32) -> bool {
+    if (low_a == low_b) || (high_a == high_b) {
+        return true;
+    }
+    if low_a < low_b {
+        if (high_a >= high_b) || (low_b <= high_a) {
+            return true;
+        }
+    }
+    if low_a > low_b {
+        if (high_a <= high_b) || (low_a <= high_b) {
+            return true;
+        }
+    }
+    false
+}
+
+fn check_pairs(re: &regex::Regex, line: &str, check: fn(i32, i32, i32, i32) -> bool) -> u32 {
     let pairs = re.captures(line).unwrap();
     let low_a: i32 = pairs["lowA"].parse().unwrap();
     let high_a: i32 = pairs["highA"].parse().unwrap();
     let low_b: i32 = pairs["lowB"].parse().unwrap();
     let high_b: i32 = pairs["highB"].parse().unwrap();
-    let result = contains(low_a, high_a, low_b, high_b);
+    let result = check(low_a, high_a, low_b, high_b);
     if result {
         1
     } else {
@@ -36,14 +53,17 @@ fn parse_pairs(re: &regex::Regex, line: &str) -> u32 {
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
-    let mut total: u32 = 0;
+    let mut part1_total: u32 = 0;
+    let mut part2_total: u32 = 0;
     let re: regex::Regex =
         Regex::new(r"(?P<lowA>\d+)-(?P<highA>\d+),(?P<lowB>\d+)-(?P<highB>\d+)").unwrap();
 
     println!("Input file: {file_path}");
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     for line in contents.lines() {
-        total += parse_pairs(&re, line);
+        part1_total += check_pairs(&re, &line, contains);
+        part2_total += check_pairs(&re, &line, overlap);
     }
-    println!("Part 1: {}", total);
+    println!("Part 1: {}", part1_total);
+    println!("Part 2: {}", part2_total);
 }
