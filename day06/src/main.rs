7c6567e450fd980ca9df06b1186d2dc254261409
Patrice Tisserand <p.tisserand@gmail.com>
2022-12-08 00:38:16 +0100

    day06: part 2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -5,17 +5,32 @@ fn find_marker_part1(stream: String) -> usize {
     let bytes = stream.as_bytes();
     let mut position: usize = 3;
     for _ in 0..bytes.len() - 4 {
-        if (bytes[position - 3] != bytes[position - 2])
-            && (bytes[position - 3] != bytes[position - 1])
-            && (bytes[position - 3] != bytes[position])
-        {
-            if (bytes[position - 2] != bytes[position - 1])
-                && (bytes[position - 2] != bytes[position])
-            {
-                if bytes[position - 1] != bytes[position] {
-                    break;
-                }
+        if !bytes[position-2..position+1].contains(&bytes[position-3]) && 
+        !bytes[position-1..position+1].contains(&bytes[position-2]) &&
+        bytes[position-1] != bytes[position] {
+            break;
+        }
+        position += 1;
+    }
+    position + 1
+}
+
+fn find_marker_part2(stream: String) -> usize {
+    let bytes = stream.as_bytes();
+    let mut position: usize = 13;
+    for _ in 0..bytes.len() - 14 {
+        let mut found = 0;
+        for i in 0..14 {
+            if bytes[position-i+1..position+1].contains(&bytes[position-i]) {
+                found += 1;
+                break;
             }
+        }
+        if bytes[position-1] == bytes[position] {
+            found = 1;
+        }
+        if found == 0{
+            break;
         }
         position += 1;
     }
@@ -28,7 +43,10 @@ fn main() {
     println!("Input file: {file_path}");
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     for line in contents.lines() {
-        let position = find_marker_part1(String::from(line));
-        println!("Part1: {}", position);
+        let position_part1 = find_marker_part1(String::from(line));
+        println!("Part1: {}", position_part1);
+        let position_part2 = find_marker_part2(String::from(line));
+        println!("Part2: {}", position_part2);
+
     }
 }
