232ade0d68c1231a7e43b208b7e9e480b54dc8f6
Patrice Tisserand <p.tisserand@gmail.com>
2022-12-13 22:06:27 +0100

    day01: using expect instead of unwrap allow to add some context

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,7 +1,4 @@
 fn main() {
-  let input = match std::fs::read_to_string("input2.txt") {
-    Ok(s) => s,
-    Err(e) => panic!("Couldn't read input.txt: {e}"),
-  };
+  let input = std::fs::read_to_string("input2.txt").expect("while reading input.txt");
   println!("{input}");
 }
