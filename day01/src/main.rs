ae89baee85f4a777cab6664b3940fbbfef688d87
Patrice Tisserand <p.tisserand@gmail.com>
2022-12-13 22:46:25 +0100

    day01: basic loop for all lines

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -3,9 +3,17 @@ fn main() -> color_eyre::Result<()>{
   // add color-eyre handler
   color_eyre::install()?;
 
-  let input = include_str!("../input.txt");
-  println!("{input}");
-
+  let input = include_str!("../input_test.txt");
+  let mut lines = input.lines();
+  loop {
+    let maybe_line = lines.next();
+    match maybe_line {
+      Some(line) => {
+        println!("Got line: {}", line);
+      }
+      None => break,
+    }
+  }
   // return a result
   Ok(())
 }
