f42f06ccfcc9ae4e5d6c1d17ebb0810e18739bce
Patrice Tisserand <p.tisserand@gmail.com>
2022-12-13 22:19:28 +0100

    day01: switch to derive(Debug) for PAthedIoError

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,13 +1,8 @@
 /// And [std::io::Error] associated with a path
+#[derive(Debug)]
 struct PathedIoError {
   path: String,
   inner: std::io::Error,
-}
-
-impl std::fmt::Debug for PathedIoError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("PathedIoError").field("path", &self.path).field("inner", &self.inner).finish()
-    }
 }
 
 fn main() {
