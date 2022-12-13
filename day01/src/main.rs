ea4984a235ff37669d70eb4aa119e3b803b62d8e
Patrice Tisserand <p.tisserand@gmail.com>
2022-12-13 22:37:24 +0100

    day01: add context to color-eyre error

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,11 +1,15 @@
+use color_eyre::eyre::Context;
+
 
 fn main() {
+  // add color-eyre handler
+  color_eyre::install().unwrap();
   let input = read_input().unwrap();
   println!("{input}");
 }
 
 fn read_input() -> color_eyre::Result<String> {
   let path = "input2.txt";
-  let input = std::fs::read_to_string(path)?;
+  let input = std::fs::read_to_string(path).wrap_err("reading input.txt")?;
   Ok(input)
 }
