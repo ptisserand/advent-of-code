b15bd410644bf327e9d33403b2343b3fd664c0a8
Patrice Tisserand <p.tisserand@gmail.com>
2022-12-13 23:00:16 +0100

    day01: compute max imperative style

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -2,15 +2,18 @@ fn main() -> color_eyre::Result<()> {
     // add color-eyre handler
     color_eyre::install()?;
 
+    let mut max = 0;
     for group in include_str!("../input_test.txt").split("\n\n") {
         let mut sum = 0;
         for line in group.lines() {
             let value = line.parse::<u64>()?;
             sum += value;
         }
-        println!("Group has sum {sum}");
+        if sum > max {
+          max = sum;
+        }
     }
-
+    println!("Part 1: {max}");
     // return a result
     Ok(())
 }
