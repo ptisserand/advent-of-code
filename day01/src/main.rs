d3f3b409df9b517d941ad82278d5911e4140d362
Patrice Tisserand <p.tisserand@gmail.com>
2022-12-13 22:28:32 +0100

    day01: use fs-err crate

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,24 +1,10 @@
-/// And [std::io::Error] associated with a path
-struct PathedIoError {
-  path: String,
-  inner: std::io::Error,
-}
-
-impl std::fmt::Debug for PathedIoError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-      write!(f, "for file {:?}: {}", self.path, self.inner)
-    }
-}
 
 fn main() {
   let input = read_input().unwrap();
   println!("{input}");
 }
 
-fn read_input() -> Result<String, PathedIoError> {
+fn read_input() -> Result<String, std::io::Error> {
   let path = "input2.txt";
-  match std::fs::read_to_string(path) {
-    Ok(s) => Ok(s),
-    Err(e) => Err(PathedIoError {path: path.into(), inner: e,})
-  }
+  fs_err::read_to_string(path)
 }
