ed435e407b31cee5304e7a2cf77f14d75a3ff832
Patrice Tisserand <p.tisserand@gmail.com>
2022-12-13 22:17:42 +0100

    day01: add PathedIoError

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,8 +1,24 @@
+/// And [std::io::Error] associated with a path
+struct PathedIoError {
+  path: String,
+  inner: std::io::Error,
+}
+
+impl std::fmt::Debug for PathedIoError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("PathedIoError").field("path", &self.path).field("inner", &self.inner).finish()
+    }
+}
+
 fn main() {
   let input = read_input().unwrap();
   println!("{input}");
 }
 
-fn read_input() -> Result<String, std::io::Error> {
-  std::fs::read_to_string("input2.txt")
+fn read_input() -> Result<String, PathedIoError> {
+  let path = "input2.txt";
+  match std::fs::read_to_string(path) {
+    Ok(s) => Ok(s),
+    Err(e) => Err(PathedIoError {path: path.into(), inner: e,})
+  }
 }
