440fb310d22f48bd47e531f24f16edd8677c99f6
Patrice Tisserand <p.tisserand@gmail.com>
2022-12-13 22:42:45 +0100

    day01: 'input.txt' is included at compilation time

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,17 +1,12 @@
-use color_eyre::eyre::Context;
 
 fn main() -> color_eyre::Result<()>{
   // add color-eyre handler
   color_eyre::install()?;
-  let input = read_input()?;
+
+  let input = include_str!("../input.txt");
   println!("{input}");
 
   // return a result
   Ok(())
 }
 
-fn read_input() -> color_eyre::Result<String> {
-  let path = "input2.txt";
-  let input = std::fs::read_to_string(path).wrap_err("reading input.txt")?;
-  Ok(input)
-}
