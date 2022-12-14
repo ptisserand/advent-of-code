f3ff258fae907d646d6d231881574ae52b07bf50
Patrice Tisserand <p.tisserand@gmail.com>
2022-12-14 10:29:47 +0100

    day01: switch to iterator 'mode'

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -2,18 +2,20 @@ fn main() -> color_eyre::Result<()> {
     // add color-eyre handler
     color_eyre::install()?;
 
-    let mut max = 0;
-    for group in include_str!("../input_test.txt").split("\n\n") {
-        let mut sum = 0;
-        for line in group.lines() {
-            let value = line.parse::<u64>()?;
-            sum += value;
-        }
-        if sum > max {
-          max = sum;
-        }
-    }
-    println!("Part 1: {max}");
+    let lines = include_str!("../input_test.txt")
+        .lines()
+        .collect::<Vec<_>>();
+    let groups = lines.split(|line| line.is_empty()).collect::<Vec<_>>();
+    let groups = groups
+        .into_iter()
+        .map(|group| {
+            group
+                .iter()
+                .map(|v| v.parse::<u64>().ok())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+    println!("Groups: {groups:?}");
     // return a result
     Ok(())
 }
