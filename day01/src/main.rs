39d9d544208bf7e5ab49b06ea00ea1b6123f5e57
Patrice Tisserand <p.tisserand@gmail.com>
2022-12-14 10:37:00 +0100

    day01: compute sum per group

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -8,6 +8,7 @@ fn main() -> color_eyre::Result<()> {
         .collect::<Vec<_>>();
     let groups = lines
         .split(|line| line.is_none())
+        .map(|group| group.iter().map(|v| v.unwrap()).sum::<u64>())
         .collect::<Vec<_>>();
     println!("Groups: {groups:?}");
     // return a result
