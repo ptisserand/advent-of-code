e592beb6a997d78fa2c455eb4452cd927203fd07
Patrice Tisserand <p.tisserand@gmail.com>
2022-12-14 10:34:18 +0100

    day01: parse u64 earlier

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -4,15 +4,10 @@ fn main() -> color_eyre::Result<()> {
 
     let lines = include_str!("../input_test.txt")
         .lines()
+        .map(|v| v.parse::<u64>().ok())
         .collect::<Vec<_>>();
     let groups = lines
-        .split(|line| line.is_empty())
-        .map(|group| {
-            group
-                .iter()
-                .map(|v| v.parse::<u64>().ok())
-                .collect::<Vec<_>>()
-        })
+        .split(|line| line.is_none())
         .collect::<Vec<_>>();
     println!("Groups: {groups:?}");
     // return a result
