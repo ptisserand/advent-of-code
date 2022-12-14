96d5add5db6b43154eda60ba8191b3b7117f9038
Patrice Tisserand <p.tisserand@gmail.com>
2022-12-14 11:19:49 +0100

    day01: try itertools::coalesce instead of batching

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -7,14 +7,16 @@ fn main() -> color_eyre::Result<()> {
     let max = include_str!("../input.txt")
         .lines()
         .map(|v| v.parse::<u64>().ok())
-        .batching(|it| {
-            let mut sum = None;
-            while let Some(Some(v)) = it.next() {
-                sum = Some(sum.unwrap_or(0) + v);
-            }
-            sum
-        }).max();
-    println!("Part1: {max:?}");
+        .coalesce(|a, b| match(a, b) {
+            (None, None) => Ok(None),
+            (None, Some(b)) => Ok(Some(b)),
+            (Some(a), Some(b)) => Ok(Some(a + b)),
+            (Some(a), None) => Err((Some(a), None)),
+        })
+        .max()
+        .flatten()
+        .unwrap_or_default();
+        println!("Part1: {max:?}");
     // return a result
     Ok(())
 }
