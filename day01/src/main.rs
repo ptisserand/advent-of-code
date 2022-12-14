bb1bc0e6ab3357f0decbbf2e422507109070cff8
Patrice Tisserand <p.tisserand@gmail.com>
2022-12-14 12:02:32 +0100

    day01: improve batching alternative with sum1 and map_while

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,35 +1,21 @@
-use std::{cmp::Reverse, collections::BinaryHeap};
+use std::cmp::Reverse;
 
-use itertools::{Itertools, FoldWhile};
+use itertools::Itertools;
 
 fn main() -> color_eyre::Result<()> {
     // add color-eyre handler
     color_eyre::install()?;
 
-    let mut group_sums = include_str!("../input.txt")
+    let answer = include_str!("../input.txt")
         .lines()
         .map(|v| v.parse::<u64>().ok())
         // consider all lines separated by 'None'
-        .batching(|it| {
-            it.fold_while(None, |acc: Option<u64>, v| match v {
-                Some(v) => FoldWhile::Continue(Some(acc.unwrap_or_default() + v)),
-                // group separator , it's done
-                None => FoldWhile::Done(acc),
-            })
-            .into_inner()
-        })
-        .map(Reverse);
+        .batching(|mut it| (&mut it).map_while(|x| x).sum1::<u64>())
+        .map(Reverse)
+        .k_smallest(3)
+        .map(|x| x.0)
+        .sum::<u64>();
 
-        let mut heap = BinaryHeap::new();
-        for init in (&mut group_sums).take(3) {
-            heap.push(init);
-        }
-        for rest in group_sums {
-            heap.push(rest);
-            heap.pop();
-        }
-        let answer = heap.into_iter().map(|Reverse(v)| v).sum::<u64>();
-        println!("Part2: {answer:?}");
-    // return a result
+    println!("Part2: {answer:?}");
     Ok(())
 }
