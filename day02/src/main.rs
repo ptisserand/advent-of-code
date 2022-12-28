2d2012065cfd8c193d94fb49097245b3b8040fa0
Patrice Tisserand <p.tisserand@gmail.com>
2022-12-28 17:34:10 +0100

    day02: parse using TryFrom & FromStr traits

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,131 +1,92 @@
 use std::env;
-use std::fs;
 use std::fmt;
+use std::fs;
+use std::str::FromStr;
 
-#[derive(PartialEq, Eq, Clone, Copy)]
-enum Shape {
+#[derive(Debug, Clone, Copy)]
+enum Move {
     Rock,
     Paper,
     Scissors,
 }
 
-impl fmt::Display for Shape {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+#[derive(Debug, Clone, Copy)]
+enum OutCome {
+    Win,
+    Draw,
+    Loss,
+}
+#[derive(Debug, Clone, Copy)]
+struct Round {
+    theirs: Move,
+    ours: Move,
+}
+
+// 'Move' parsing
+impl TryFrom<char> for Move {
+    type Error = color_eyre::Report;
+
+    fn try_from(c: char) -> Result<Self, Self::Error> {
+        match c {
+            'A' | 'X' => Ok(Move::Rock),
+            'B' | 'Y' => Ok(Move::Paper),
+            'C' | 'Z' => Ok(Move::Scissors),
+            _ => Err(color_eyre::eyre::eyre!("not a valid move: {c:?}")),
+        }
+    }
+}
+
+// 'Round' parsing
+impl FromStr for Round {
+    type Err = color_eyre::Report;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut chars = s.chars();
+        let (Some (theirs), Some(' '), Some(ours), None) = (chars.next(), chars.next(), chars.next(), chars.next()) else {
+            return Err(color_eyre::eyre::eyre!("expected <theirs>SP<ours>EOF, got {s:?}"));
+        };
+
+        Ok(Self {
+            theirs: theirs.try_into()?,
+            ours: ours.try_into()?,
+        })
+    }
+}
+
+impl Move {
+    fn inherent_points(self) -> usize {
         match self {
-            Shape::Rock => write!(f, "rock"),
-            Shape::Paper => write!(f, "paper"),
-            Shape::Scissors => write!(f, "scissors"),
+            Move::Rock => 1,
+            Move::Paper => 2,
+            Move::Scissors => 3,
+        }
+    }
+
+    fn outcome(self, theirs: Move) -> OutCome {
+        match (self, theirs) {
+            (Move::Rock, Move::Rock) => OutCome::Draw,
+            (Move::Rock, Move::Paper) => OutCome::Loss,
+            (Move::Rock, Move::Scissors) => OutCome::Win,
+            (Move::Paper, Move::Rock) => OutCome::Win,
+            (Move::Paper, Move::Paper) => OutCome::Draw,
+            (Move::Paper, Move::Scissors) => OutCome::Loss,
+            (Move::Scissors, Move::Rock) => OutCome::Loss,
+            (Move::Scissors, Move::Paper) => OutCome::Win,
+            (Move::Scissors, Move::Scissors) => OutCome::Draw,
         }
     }
 }
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
 
-fn win(opponent: &Shape, me: &Shape) -> i32 {
-    let mut winner = 0;
-    if *opponent == *me {
-        winner = 0;
-        return winner;
-    }
-    if *opponent == Shape::Rock {
-        if *me == Shape::Paper {
-            winner = 1;
-        } else {
-            winner = -1;
-        }
-    }
-    if *opponent == Shape::Paper {
-        if *me == Shape::Scissors {
-            winner = 1;
-        } else {
-            winner = -1;
-        }
-    }
-    if *opponent == Shape::Scissors {
-        if *me == Shape::Rock {
-            winner = 1;
-        } else {
-            winner = -1;
-        }
-    }
-    return winner;
-}
-
-fn strategy_part1(line: &str) -> (Shape, Shape) {
-    let opponent = match line.as_bytes()[0] {
-        b'A' => Shape::Rock,
-        b'B' => Shape::Paper,
-        b'C' => Shape::Scissors,
-        _ => Shape::Rock,
-    };
-    let me = match line.as_bytes()[2] {
-        b'X' => Shape::Rock,
-        b'Y' => Shape::Paper,
-        b'Z' => Shape::Scissors,
-        _ => Shape::Rock,
-    };
-    (opponent, me)
-}
-
-fn loser(opponent: &Shape) -> Shape {
-    match *opponent {
-        Shape::Rock => Shape::Scissors,
-        Shape::Paper => Shape::Rock,
-        Shape::Scissors => Shape::Paper,
-    }
-}
-
-fn winner(opponent: &Shape) -> Shape {
-    match *opponent {
-        Shape::Rock => Shape::Paper,
-        Shape::Paper => Shape::Scissors,
-        Shape::Scissors => Shape::Rock,
-    }
-}
-
-
-fn strategy_part2(line: &str) -> (Shape, Shape) {
-    let opponent = match line.as_bytes()[0] {
-        b'A' => Shape::Rock,
-        b'B' => Shape::Paper,
-        b'C' => Shape::Scissors,
-        _ => Shape::Rock,
-    };
-    let me = match line.as_bytes()[2] {
-        b'X' => loser(&opponent),
-        b'Y' => opponent, // draw
-        b'Z' => winner(&opponent),
-        _ => Shape::Rock,
-    };
-    (opponent, me)
-}
-
-fn apply_strategy(contents: &String, strategy: fn(&str) -> (Shape, Shape)) -> i32 {
-    let mut total = 0;
-    for line in (*contents).lines() {
-        let round = strategy(line);
-        let score = win(&round.0, &round.1);
-        total += match round.1 {
-            Shape::Rock => 1,
-            Shape::Paper => 2,
-            Shape::Scissors => 3,
-        };
-        total += match score {
-            -1 => 0,
-            0 => 3,
-            1 => 6,
-            _ => 0,
-        };
-    }
-    total
-}
-
-
-fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
     println!("Input file: {file_path}");
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-    let mut score =  apply_strategy(&contents, strategy_part1);
-    println!("Score part 1: {}", score);
-    score = apply_strategy(&contents, strategy_part2);
-    println!("Score part 2: {}", score);
+    for round in contents.lines().map(|line| line.parse::<Round>()) {
+        let round = round?;
+        println!("{round:?}");
+    };
+
+    Ok(())
 }
