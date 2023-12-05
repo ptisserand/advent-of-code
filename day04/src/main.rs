0bfaa24daa7b4b8307c382bfd919b74967c2cddf
Patrice Tisserand <p.tisserand@gmail.com>
2023-12-05 11:01:10 +0100

    day04: part2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -22,16 +22,35 @@ fn parse_input(line: &str) -> (Vec<u32>, Vec<u32>) {
 
 fn matching_numbers(line: &str) -> usize {
     let (winnings, choices) = parse_input(line);
-    let found = choices.iter().filter(|x| winnings.contains(x)).count();
-    if found > 0 {
-        1 << (found - 1)
-    } else {
-        0
-    }
+    choices.iter().filter(|x| winnings.contains(x)).count()
 }
 
 fn part1(contents: &str) -> usize {
-    contents.lines().map(matching_numbers).sum()
+    contents
+        .lines()
+        .map(matching_numbers)
+        .map(|x| if x > 0 { 1 << (x - 1) } else { 0 })
+        .sum()
+}
+
+fn part2(contents: &str) -> usize {
+    let scores = contents.lines().map(matching_numbers);
+    let nb_lines = contents.lines().count();
+    let mut cards: Vec<usize> = vec![1; nb_lines];
+
+    for (i, score) in scores.enumerate() {
+        let value = cards[i];
+        let rem = if i + score <= nb_lines {
+            score
+        } else {
+            nb_lines - i
+        };
+        (0..rem).for_each(|j| {
+            cards[i + 1 + j] += value;
+        });
+    }
+
+    cards.iter().sum()
 }
 
 fn main() -> color_eyre::Result<()> {
@@ -42,6 +61,8 @@ fn main() -> color_eyre::Result<()> {
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let value_part1 = part1(&contents);
     println!("Part1: {}", value_part1);
+    let value_part2 = part2(&contents);
+    println!("Part2: {}", value_part2);
     Ok(())
 }
 
@@ -92,12 +113,30 @@ mod tests {
 
     #[test]
     fn test_matching_numbers() {
-        assert_eq!(8, matching_numbers("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"));
-        assert_eq!(2, matching_numbers("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19"));
-        assert_eq!(2, matching_numbers("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1"));
-        assert_eq!(1, matching_numbers("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83"));
-        assert_eq!(0, matching_numbers("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36"));
-        assert_eq!(0, matching_numbers("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"));
+        assert_eq!(
+            4,
+            matching_numbers("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53")
+        );
+        assert_eq!(
+            2,
+            matching_numbers("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19")
+        );
+        assert_eq!(
+            2,
+            matching_numbers("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1")
+        );
+        assert_eq!(
+            1,
+            matching_numbers("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83")
+        );
+        assert_eq!(
+            0,
+            matching_numbers("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36")
+        );
+        assert_eq!(
+            0,
+            matching_numbers("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11")
+        );
     }
 
     #[test]
@@ -109,5 +148,16 @@ Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
         assert_eq!(13, part1(input));
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = r"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+        assert_eq!(30, part2(input));
     }
 }
